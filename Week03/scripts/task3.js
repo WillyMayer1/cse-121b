9f9171b84d8d9b5ed98ca1683599161b143fa06d
WillyMayer1 <willianmayercl@gmail.com>
2022-10-01 03:29:44 -0300

    checkpoint

--- Week03/scripts/task3.js
+++ Week03/scripts/task3.js
@@ -3,15 +3,20 @@
 /* FUNCTIONS */
 
 // Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
-
+function add(number1, number2) //Ok
 // Step 2: In the function, return the sum of the parameters number1 and number2
+{return number1 + number2}; //OK
+// Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
+function addNumbers() {
+let addend1 = Number(document.querySelector("#addend1").value);
+let addend2 = Number(document.querySelector("#addend2").value);
 
-// Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
-
+const results = add(addend1, addend2);
 // Step 4: Assign the return value to an HTML form element with an ID of sum
-
+document.getElementById("sum").value = results;
+}
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
-
+document.querySelector("#addNumbers").addEventListener("click", addNumbers);
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
 
 // Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
