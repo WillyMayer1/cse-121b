3b90a8cdafed5e0bb8fc18b6cf35194cd8c43ba1
WillyMayer1 <willianmayercl@gmail.com>
2022-10-06 01:21:17 -0300

    checkpoint

--- week04/scripts/task4.js
+++ week04/scripts/task4.js
@@ -5,9 +5,9 @@
 // Step 1: Declare a new variable to hold information about yourself
 let studentInfo = {
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
-name: "Willian",
+"name": "Willian",
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
-photo: "images/me.jpg",
+"photo": "images/me.jpg",
 // Step 4: Add another property named favoriteFoods with a value of an array of your favorite foods as strings ( hint: [] )
 favoriteFoods: ["Empanadas", "Pasta"],
 // Step 5: Add another property named hobbies with a value of an array of your hobbies as strings
@@ -25,11 +25,11 @@ placesLived: [
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
-
+document.querySelector("#name").innerHTML = studentInfo["name"];
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
-
+document.querySelector("#photo").src = studentInfo["photo"];
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
-
+document.querySelector("#photo").innerHTML = studentInfo["photo"];
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
 
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
