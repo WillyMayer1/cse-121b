5a637b6bfdb35a2a8f1bd638e59dc375126ad081
WillyMayer1 <willianmayercl@gmail.com>
2022-10-06 14:50:50 -0300

    checkpoint

--- week04/scripts/task4.js
+++ week04/scripts/task4.js
@@ -9,9 +9,9 @@ let studentInfo = {
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
 "photo": "images/me.jpg",
 // Step 4: Add another property named favoriteFoods with a value of an array of your favorite foods as strings ( hint: [] )
-favoriteFoods: ["Empanadas", "Pasta"],
+"favoriteFoods": ["banana", " apple", " watermelon"],
 // Step 5: Add another property named hobbies with a value of an array of your hobbies as strings
-hobbies: ["Videogames", "Meet with friends"],
+"hobbies": ["Videogames", "Meet with friends"],
 // Step 6: Add another property named placesLived with a value of an empty array
 placesLived: [
 // Step 7: Inside of the empty array above, add a new object with two properties: place and length and values of an empty string
@@ -31,13 +31,24 @@ document.querySelector("#photo").src = studentInfo["photo"];
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
 document.querySelector("#photo").innerHTML = studentInfo["photo"];
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+let favFoodsList = document.querySelector("#favorite-foods");
+let favFoods = studentInfo["favoriteFoods"]; 
 
+favFoods.forEach(favFood => {
+    let listItem = document.createElement("li");
+    listItem.textContent = favFood;
+    
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
-
+    favFoodsList.append(listItem)});
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
-
+let hobbiesList = document.querySelector("#hobbies");
+let hobbies = studentInfo["hobbies"];
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
-
+hobbies.forEach(hobbie => {
+    let listItem = document.createElement("li");
+    listItem.textContent = hobbie;
+    hobbiesList.append(listItem);
+});
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
